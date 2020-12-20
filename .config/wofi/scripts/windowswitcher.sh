a016888cb91c029c64a99d5bb763f0a84542d4f5
Transistor4aCPU <transistor@maxxhardware.de>
2020-12-20 23:03:50 +0100

    Improved wofi

--- .config/wofi/scripts/windowswitcher.sh
+++ .config/wofi/scripts/windowswitcher.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-id=$(swaymsg -t get_tree | jq -r '.nodes[].nodes[] | if .nodes then [recurse(.nodes[])] else [] end + .floating_nodes | .[] | select(.nodes==[]) | (.name + " (" + (.id | tostring) + ")")' | sed -n '1!p' | wofi --show dmenu -L 10 -O alphabetical -b -i | grep -o -P "(?<=\().*(?=\))")
+id=$(swaymsg -t get_tree | jq -r '.nodes[].nodes[] | if .nodes then [recurse(.nodes[])] else [] end + .floating_nodes | .[] | select(.nodes==[]) | (.name + " (" + (.id | tostring) + ")")' | sed -n '1!p' | sed '/^1/d' | sed '/^2/d' | sed '/^3/d' | sed '/^4/d' | sed '/^5/d' | sed '/^6/d' | sed '/^7/d' | sed '/^8/d' | sed '/^9/d' | sed '/^0/d' | wofi --show dmenu -L 10 -O alphabetical -b -i | grep -o -P "(?<=\().*(?=\))")
 swaymsg "[con_id=$id]" focus
 exit

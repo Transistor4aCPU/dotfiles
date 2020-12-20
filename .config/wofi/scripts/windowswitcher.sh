25abd55138f8c21e8a82b9a03a785478b9da962a
Transistor4aCPU <transistor@maxxhardware.de>
2020-12-20 22:54:47 +0100

    Updated dotfiles

--- .config/wofi/scripts/windowswitcher.sh
+++ .config/wofi/scripts/windowswitcher.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-id=$(swaymsg -t get_tree | jq -r '.nodes[].nodes[] | if .nodes then [recurse(.nodes[])] else [] end + .floating_nodes | .[] | select(.nodes==[]) | (.name + " (" + (.id | tostring) + ")")' | sed -n '1!p' | wofi --show dmenu -L 10 | grep -o '[[:digit:]]*')
+id=$(swaymsg -t get_tree | jq -r '.nodes[].nodes[] | if .nodes then [recurse(.nodes[])] else [] end + .floating_nodes | .[] | select(.nodes==[]) | (.name + " (" + (.id | tostring) + ")")' | sed -n '1!p' | wofi --show dmenu -L 10 -O alphabetical -b -i | grep -o -P "(?<=\().*(?=\))")
 swaymsg "[con_id=$id]" focus
 exit

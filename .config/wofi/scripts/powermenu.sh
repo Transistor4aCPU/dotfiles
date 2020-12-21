e110b10529054857ce402d82ac81ff4df41e8ab2
Transistor4aCPU <transistor@maxxhardware.de>
2020-12-22 00:59:50 +0100

    Improved powermenu

--- .config/wofi/scripts/powermenu.sh
+++ .config/wofi/scripts/powermenu.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-chosen="$(echo -e "Shutdown\nReboot\nLock\nSuspend\nHibernate\nLogout" | wofi -d -L 6 -W 200 -H 200)"
+chosen="$(echo -e "Shutdown\nReboot\nLock\nSuspend\nHibernate\nLogout" | wofi -d -L 6 -W 200 -H 200 -p 'Powermenu')"
 case $chosen in
     Shutdown) mpv ~/.config/sway/sounds/shutdowninitiated.wav && systemctl poweroff ;;
     Reboot) systemctl reboot ;;

08f12f18c88f4bece48fe29d4d16cc8e4be1c42c
Transistor4aCPU <transistor@maxxhardware.de>
2020-12-21 14:59:37 +0100

    Improved websearch

--- .config/wofi/scripts/web-search.sh
+++ .config/wofi/scripts/web-search.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-SEARCH="$(wofi -d)"
-SUGGESTION=$(curl https://ac.ecosia.org/?q=${SEARCH} | grep -o -P '(?<="suggestions"\:\[").*(?="\]\})' | sed 's/","/\n/g')
-CHOOSEN=$(echo "$SUGGESTION" | wofi -d | sed 's/ /+/g')
-xdg-open "https://www.ecosia.org/search?q=${CHOOSEN}"
+SEARCH="$(wofi -d -L 1)"
+SUGGESTION=$(curl https://ac.ecosia.org/?q=${SEARCH} | grep -o -P '(?<="suggestions"\:\[").*(?="\]\})' | sed 's/","/\n/g' | wofi -d -L 8 | sed 's/ /+/g')
+xdg-open "https://www.ecosia.org/search?q=${SUGGESTION}"
 exit

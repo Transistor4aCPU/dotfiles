7c75614ed1f29f1a85da6fd23a40b2a273d812f2
Transistor4aCPU <transistor@maxxhardware.de>
2020-12-21 16:58:07 +0100

    Added icons to wofi

--- .config/wofi/scripts/web-search.sh
+++ .config/wofi/scripts/web-search.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SEARCH="$(wofi -d -L 1 | sed 's/ /+/g')"
-SUGGESTION=$(curl https://ac.ecosia.org/?q=${SEARCH} | grep -o -P '(?<="suggestions"\:\[").*(?="\]\})' | sed 's/","/\n/g' | wofi -d -L 8 | sed 's/ /+/g')
+SUGGESTION=$(curl https://ac.ecosia.org/?q=${SEARCH} | grep -o -P '(?<="suggestions"\:\[").*(?="\]\})' | sed 's/","/\n/g' | wofi -d -L 8 -p "Search the web..." | sed 's/ /+/g')
 xdg-open "https://www.ecosia.org/search?q=${SUGGESTION}"
 exit

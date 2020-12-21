a49609a6fa014a076a367a030683a27a45eefbe5
Transistor4aCPU <transistor@maxxhardware.de>
2020-12-21 16:38:00 +0100

    Added system sounds

--- .config/wofi/scripts/powermenu.sh
+++ .config/wofi/scripts/powermenu.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 chosen="$(echo -e "Shutdown\nReboot\nLock\nSuspend\nHibernate\nLogout" | wofi -d -L 6 -W 200 -H 200)"
 case $chosen in
-    Shutdown) systemctl poweroff ;;
+    Shutdown) mpv ~/.config/sway/sounds/shutdowninitiated.wav && systemctl poweroff ;;
     Reboot) systemctl reboot ;;
     Lock) swaylock -f -c 000000 ;;
     Suspend) systemctl suspend ;;

0ec7a489e3138ea2583b59f4be20f6c94bd425fa
Transistor4aCPU <transistor@maxxhardware.de>
2020-12-23 12:06:15 +0100

    Added grimshot as requirement

--- requirementsandrecommendations.sh
+++ requirementsandrecommendations.sh
@@ -3,6 +3,6 @@ sudo pacman -S sway swaybg swayidle swaylock waybar mako alacritty wofi playerct
 # sudo pacman -S gedit qt5-wayland qt6-wayland sddm gnome-passwordsafe
 sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git; cd yay; makepkg -si
-yay -S wlsunset wob macho
+yay -S wlsunset wob macho grimshot
 # yay -S gfeeds
 exit

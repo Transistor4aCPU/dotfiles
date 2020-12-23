114b7b4e06d4911bf087aa9b981ecac129b5e972
Transistor4aCPU <transistor@maxxhardware.de>
2020-12-23 14:47:26 +0100

    Implement xdg autostart

--- requirementsandrecommendations.sh
+++ requirementsandrecommendations.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo pacman -S sway swaybg swayidle swaylock waybar mako alacritty wofi playerctl brightnessctl wl-clipboard xorg-xwayland pamixer
+sudo pacman -S sway swaybg swayidle swaylock waybar mako alacritty wofi playerctl brightnessctl wl-clipboard xorg-xwayland pamixer dex
 # sudo pacman -S gedit qt5-wayland qt6-wayland sddm gnome-passwordsafe
 sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git; cd yay; makepkg -si

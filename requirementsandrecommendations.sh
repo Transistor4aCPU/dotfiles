532e81eff8bdb2da156777c9ba82c0969925a599
Transistor4aCPU <transistor@maxxhardware.de>
2020-12-30 16:20:20 +0100

    Added pavucontrol

--- requirementsandrecommendations.sh
+++ requirementsandrecommendations.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo pacman -S sway swaybg swayidle swaylock waybar mako alacritty wofi playerctl brightnessctl wl-clipboard xorg-xwayland pamixer dex curl
+sudo pacman -S sway swaybg swayidle swaylock waybar mako alacritty wofi playerctl brightnessctl wl-clipboard xorg-xwayland pamixer dex curl pavucontrol
 # sudo pacman -S qt5-wayland qt6-wayland
 sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git; cd yay; makepkg -si

cb6911148c4a9d22b928a6a688321229f35d15e8
Transistor4aCPU <59773835+Transistor4aCPU@users.noreply.github.com>
2021-01-24 14:51:36 +0100

    Update translatorensys.sh

--- .config/wofi/scripts/translatorensys.sh
+++ .config/wofi/scripts/translatorensys.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+TARGET="$(echo "$LANG" | grep -o ^..)"
 SEARCH="$(wofi -d -L 1 | sed 's/ /+/g')"
-SUGGESTION=$(curl "https://libretranslate.com/translate" -H "Content-Type: application/json" -d "{\"q\": \"${SEARCH}\", \"source\": \"en\", \"target\": \"${LANGUAGE}\"}" | sed 's/^{"translatedText":"//g' | rev | sed 's/^}"//g'| rev | sed 's/+/ /g')
+SUGGESTION=$(curl "https://libretranslate.com/translate" -H "Content-Type: application/json" -d "{\"q\": \"${SEARCH}\", \"source\": \"en\", \"target\": \"${TARGET}\"}" | sed 's/^{"translatedText":"//g' | rev | sed 's/^}"//g'| rev | sed 's/+/ /g')
 wl-copy "$SUGGESTION"
 exit

dd5f8c058c82a5504c3ae614fd440c705bcc9576
Transistor4aCPU <59773835+Transistor4aCPU@users.noreply.github.com>
2021-01-24 14:52:17 +0100

    Update translatorsysen.sh

--- .config/wofi/scripts/translatorsysen.sh
+++ .config/wofi/scripts/translatorsysen.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+SOURCE="$(echo "$LANG" | grep -o ^..)"
 SEARCH="$(wofi -d -L 1 | sed 's/ /+/g')"
-SUGGESTION=$(curl "https://libretranslate.com/translate" -H "Content-Type: application/json" -d "{\"q\": \"${SEARCH}\", \"source\": \"${LANGUAGE}\", \"target\": \"en\"}" | sed 's/^{"translatedText":"//g' | rev | sed 's/^}"//g'| rev | sed 's/+/ /g')
+SUGGESTION=$(curl "https://libretranslate.com/translate" -H "Content-Type: application/json" -d "{\"q\": \"${SEARCH}\", \"source\": \"${SOURCE}\", \"target\": \"en\"}" | sed 's/^{"translatedText":"//g' | rev | sed 's/^}"//g'| rev | sed 's/+/ /g')
 wl-copy "$SUGGESTION"
 exit

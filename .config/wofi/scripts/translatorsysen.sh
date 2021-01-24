fd6a05d684b8abaf83272c78c672ffe772347b1e
Transistor4aCPU <59773835+Transistor4aCPU@users.noreply.github.com>
2021-01-24 14:30:21 +0100

    Add files via upload

---
+++ .config/wofi/scripts/translatorsysen.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+SEARCH="$(wofi -d -L 1 | sed 's/ /+/g')"
+SUGGESTION=$(curl "https://libretranslate.com/translate" -H "Content-Type: application/json" -d "{\"q\": \"${SEARCH}\", \"source\": \"${LANGUAGE}\", \"target\": \"en\"}" | sed 's/^{"translatedText":"//g' | rev | sed 's/^}"//g'| rev | sed 's/+/ /g')
+wl-copy "$SUGGESTION"
+exit
